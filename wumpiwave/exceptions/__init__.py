5e37000e23893549a34ad53b36d7a56636e02bc5
lea361 <lea.as_@outlook.de>
2026-07-23 23:28:05 +0200

    feat: initialize exceptions package
    Create the exceptions package with documentation and an empty public export list for the upcoming WumpiWave exception hierarchy.

---
+++ wumpiwave/exceptions/__init__.py
@@ -0,0 +1,16 @@
+"""
+Public exceptions raised throughout WumpiWave.
+
+This package contains the exception hierarchy shared by media queries,
+providers, resolvers, playback queues, voice backends, and media players.
+
+Attributes:
+    None
+
+Methods:
+    None
+"""
+
+from __future__ import annotations
+
+__all__: tuple[str, ...] = ()
